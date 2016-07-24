8c670d8c73a4752efa879fef330cbe6bd9eb7f2c
Nate Welling <nate@natewelling.com>
2016-07-24 10:28:43 +0800

    add fullname, description to User, add configur_permitted_parameters method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def get_avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "htts://www.gravatar.com/avatar/#{gravater_id}.jpg?d=identical&s=150"
+  end
 end
