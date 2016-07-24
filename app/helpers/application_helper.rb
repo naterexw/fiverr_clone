7b1458d56661796f86442d5dfa990e19a43e6219
Nate Welling <nate@natewelling.com>
2016-07-24 10:50:48 +0800

    add default gravtar image if user gravatar doesn't exist

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
-  def get_avatar_url(user)
+  def get_avatar_url(user, size)
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=150"
+    default_url = "http://www.gravatar.com/avatar"
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identical&s=#{size}&d=#{CGI::escape(default_url)}"
   end
 end
