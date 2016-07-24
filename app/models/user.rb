799e0286c6051b1cef54906f750c04257561ea1e
Nate Welling <nate@natewelling.com>
2016-07-24 13:33:36 +0800

    integrate Omniauth-facebook with Devise (User model, migration)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,28 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   validates_presence_of :fullname, length: {maximum: 50}
   validates :description, presence: false, length: {maximum: 1500}
 
   has_many :services
+
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+    if user
+      return user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.fullname = auth.info.name
+        user.email = auth.info.email
+        user.description = auth.info.description
+        user.image = auth.info.image
+        user.password = Devise.friendly_token[0,20]
+      end
+    end
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160723113706) do
+ActiveRecord::Schema.define(version: 20160724053240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(version: 20160723113706) do
     t.datetime "updated_at",                          null: false
     t.string   "fullname"
     t.string   "description"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
