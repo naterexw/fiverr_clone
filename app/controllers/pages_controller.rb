6bad902912762f2ca4923362cdbc38f58d0b0f80
Nate Welling <nate@natewelling.com>
2017-01-26 15:26:10 +0700

    create landing.html.erb layout, update for services

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  layout "landing"
+
   def home
+    @services = Service.all
   end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -5,14 +5,14 @@ class Service < ActiveRecord::Base
   has_many :reviews
 
   has_attached_file :image, styles: {medium: "300x300>", small: "230x140>", thumb: "100x100>"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
-  validates_presence_of :title, length: {maximum:80}
-  validates_presence_of :description, length: {maximum:1200}
+  validates_presence_of :title, length: { maximum: 80 }
+  validates_presence_of :description, length: { maximum: 1200 }
   validates_presence_of :price
   validates_presence_of :delivery_time
   validates_presence_of :revisions
-  validates_presence_of :requirements, length: {maximum:450}
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_presence_of :requirements, length: { maximum: 450 }
 
   def average_rating
     reviews.count == 0 ? 0 : reviews.average(:star).round(2)
