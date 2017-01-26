240013c9cf5d0e0596b7d887062d50b88fc75a2a
Nate Welling <nate@natewelling.com>
2017-01-26 19:13:32 +0700

    add devise secret key to devise.rb initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -264,4 +264,5 @@ Devise.setup do |config|
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
   config.omniauth :facebook, ENV["facebook_app_id"], ENV["facebook_app_secret"], scope: "email", info_fields: "email, name"
+  config.secret_key = ENV["DEVISE_SECRET_KEY"]
 end
